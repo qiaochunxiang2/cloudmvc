d7b73ad25199921776a054656439accdd5363980
qiaochxlc-s <qiaochxlc-s@inspur.com>
2020-04-19 22:12:41 +0800

    feat： 博客管理

---
+++ src/main/java/com/sk/cloudmvc/dao/BlogMapper.java
@@ -0,0 +1,44 @@
+package com.sk.cloudmvc.dao;
+
+import com.sk.cloudmvc.model.Blog;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author qiaochunxiang
+ * @date 2020/4/19 21:12
+ */
+@Repository
+public interface BlogMapper {
+
+    /**
+     * 博客发布
+     *
+     * @param jsonData 博客内容
+     * @return boolean
+     * @author qiaochunxiang
+     * @date 21:14 2020/4/19
+     **/
+    boolean publish(Map<String, Object> jsonData);
+
+    /**
+     * 查询所有博客
+     *
+     * @return java.util.List<com.sk.cloudmvc.model.Blog>
+     * @author qiaochunxiang
+     * @date 21:14 2020/4/19
+     **/
+    List<Blog> findAll();
+
+    /**
+     * 查询用户所有博客
+     *
+     * @param uid 用户id
+     * @return java.util.List<com.sk.cloudmvc.model.Blog>
+     * @author qiaochunxiang
+     * @date 21:15 2020/4/19
+     **/
+    List<Blog> personAll(String uid);
+}
---
+++ src/main/java/com/sk/cloudmvc/model/Blog.java
@@ -0,0 +1,32 @@
+package com.sk.cloudmvc.model;
+
+import lombok.Data;
+
+/**
+ * @author qiaochunxiang
+ * @date 2020/4/19 21:09
+ */
+@Data
+public class Blog {
+    /**
+     * 博客id
+     */
+    private String id;
+
+    /**
+     * 博客标题
+     */
+    private String title;
+
+    /**
+     * 博客内容
+     */
+    private String content;
+
+
+    /**
+     * 用户资料
+     */
+    private UserInformation userInformation;
+
+}
